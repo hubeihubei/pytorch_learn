34aeb16bc366eafecc7b3f649d12eedeb6088f59
wangyang <isyang.wang@gmail.com>
2018-01-17 23:12:51 +0800

    s

---
+++ cat_vs_dog/feature_extraction.py
@@ -0,0 +1,91 @@
+import argparse
+import os
+
+import h5py
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import torch.utils.data as Data
+from cat_vs_dog.feature_net import feature_net
+
+parse = argparse.ArgumentParser()
+parse.add_argument('--model', required=True, help='vgg,inceptionv3,resnet152')
+parse.add_argument('--phase', required=True, help='train,val')
+parse.add_argument('--bs', required=True, help='batch_size', default=32)
+opt = parse.parse_args()
+
+BATCH_SIZE = 32
+
+transform_compose = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(299),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(320),
+        transforms.CenterCrop(299),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+}
+root='/home/wangyang/IdeaProjects/pytorch_learn/cat_and_dog/'
+data_folder = {
+    "train": ImageFolder(root=root+"train/", transform=transform_compose['train']),
+    'val': ImageFolder(root=root+"val/", transform=transform_compose['val'])
+}
+
+dataLoader = {
+    'train': Data.DataLoader(
+        dataset=data_folder['train'],
+        batch_size=BATCH_SIZE,
+        # 提取特征向量时，数据是不能随机打乱的，因为使用多个模型，每次随机大乱斗会造成标签混乱
+        shuffle=False,
+        num_workers=4
+    ),
+    'val': Data.DataLoader(
+        dataset=data_folder['val'],
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4
+    )
+}
+
+use_gpu = torch.cuda.is_available()
+
+
+def CreateFeature(model, phase, outpath='.'):
+    if use_gpu:
+        net = feature_net(model).cuda()
+    else:
+        net = feature_net(model)
+
+    feature_map = torch.FloatTensor()
+    label_map = torch.LongTensor()
+    for step, (b_x, b_y) in enumerate(dataLoader['train']):
+        if use_gpu:
+            b_x = Variable(b_x).cuda()
+            b_y = Variable(b_y).cuda()
+        else:
+            b_x = Variable(b_x)
+            b_y = Variable(b_y)
+        out = net(b_x)
+        feature_map = torch.cat((feature_map, out.cpu().data), 0)
+        label_map = torch.cat((label_map, b_y.cpu().data), 0)
+
+    feature_map = feature_map.numpy()
+    label_map = label_map.numpy()
+
+    fileName = "_feature_'{}.hd5f".format(model)
+    h5py_path = os.path.join(outpath, phase) + fileName
+    with h5py.File(h5py_path, 'w') as h:
+        h.create_dataset('data', feature_map)
+        h.create_dataset('label', label_map)
+
+
+CreateFeature(opt.model, opt.phase)
---
+++ cat_vs_dog/feature_net.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+from torchvision.datasets import ImageFolder
+from torch.autograd import Variable
+import os
+# vgg = models.vgg19()
+# print(list(vgg.children()))
+# inceptionv3 = models.resnet152()
+# l = list(inceptionv3.children())
+# feature = nn.Sequential(*list(inceptionv3.children()))
+# print(feature)
+
+# 特征提取预训练网络
+class feature_net(nn.Module):
+    def __init__(self, model):
+        super(feature_net, self).__init__()
+
+        if model == 'vgg':
+            vgg = models.vgg19(pretrained=True)
+            # 舍弃最后一层（全连接层）
+            self.feature = nn.Sequential(*list(vgg.children())[:-1])
+            # 在最后加一层平均池化层，将结果转为特征向量
+            self.feature.add_module("global average", nn.AvgPool2d(9))
+        elif model == 'inceptionv3':
+            inceptionv3 = models.inception_v3(pretrained=True)
+            # 可变参数入参，如果已经有list或tuple则在前面加*
+            self.feature = nn.Sequential(*list(inceptionv3.children())[:-1])
+            # inception_v3的第13层也是全连接，需要去除
+            self.feature._modules.pop("13")
+            self.feature.add_module("global average", nn.AvgPool2d())
+        elif model == 'resnet152':
+            resnet = models.resnet152(pretrained=True)
+            # resnet 倒数第二层为 AvgPool2d(),所以不需要再加
+            self.feature = nn.Sequential(*list(resnet.children())[:-1])
+
+    def forward(self, x):
+        x=self.feature(x)
+        x=x.view(x.size(0),-1)
+        return x
+
+
+# 全连接分类层
+class classifier(nn.Module):
+    def __init__(self,dim_in,n_classes):
+        super(classifier, self).__init__()
+        self.layer1=nn.Sequential(
+            nn.Linear(dim_in,1000),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Linear(1000,n_classes)
+        )
+
+    def forward(self, x):
+        x=self.layer1(x)
+        return x
+
+
