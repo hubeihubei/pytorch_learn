1d2e18a09f0129e0cb2c4149e0315f13b484ea13
wangyang <isyang.wang@gmail.com>
2018-01-19 17:30:03 +0800

    'catdog'

--- pytorch_step2/cat_and_dog.py
+++ pytorch_step2/cat_and_dog.py
@@ -46,13 +46,13 @@ def train():
             transforms.RandomSizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
             transforms.Scale(320),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
     }
 
